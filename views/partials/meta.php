21159381b9ff7dee7a4a6dffccd4e111c94f8662
4141gtppuKovuKov# <joelandreu00@gmail.com>
2024-05-13 20:25:04 +0200

    Cambio meta description

--- views/partials/meta.php
+++ views/partials/meta.php
@@ -1,3 +1,3 @@
 <!-- begin: views/partials/meta.php -->
-<meta name="description" content="♣ BlackCloverdle ♣ es un juego de adivinar personajes de Black Clover diariamente.">
+<meta name="description" content="♣ BlackCloverdle ♣ es un juego diario de adivinar personajes de Black Clover diariamente.">
 <!-- end: views/partials/meta.php -->
