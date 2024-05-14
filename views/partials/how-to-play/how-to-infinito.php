55849c5f3f363f580dd431804306900755bc69b1
4141gtppuKovuKov# <joelandreu00@gmail.com>
2024-05-14 18:51:33 +0200

    Update del how to play infinito

--- views/partials/how-to-play/how-to-infinito.php
+++ views/partials/how-to-play/how-to-infinito.php
@@ -1,1 +1,37 @@
-<?php require_once PARTIALS_DIR . "how-to-play/how-to-clasico.php";
+<div id="how_to_play" class="howtoplay">
+    <div class="howtoplay__container">
+        <div class="howtoplay__heading modal-heading">🤔 Cómo jugar ❓</div>
+        <div class="howtoplay__wrapper">
+            <p>
+                El objetivo del juego es simple: adivinar el personaje oculto.
+                Tienes 6 intentos para adivinarlo. Una vez que lo adivinas puedes
+                volver a jugar tantas veces como quieras.
+            </p>
+            <p>
+                Cada intento debe ser un personaje válido. En cada ronda, el juego
+                pinta cada letra de un color, indicando si esa letra se encuentra
+                o no en la palabra y si se encuentra en la posición correcta.
+            </p>
+            <ol>
+                <li>
+                    <span class="green">C</span>
+                    VERDE significa que la letra está en la palabra y en la
+                    posición CORRECTA.
+                </li>
+                <li>
+                    <span class="red">O</span>
+                    ROJO significa que la letra letra está presente en la palabra
+                    pero en la posición INCORRECTA.
+                </li>
+                <li>
+                    <span class="grey">B</span>
+                    GRIS significa que la letra letra NO está presente en la palabra.
+                </li>
+            </ol>
+        </div>
+        <div class="howtoplay__footer">
+            <div class="howtoplay__close btn btn__primary btn__primary--ghost">Cerrar</div>
+        </div>
+    </div>
+</div>
+<div class="howtoplay__bg"></div>
